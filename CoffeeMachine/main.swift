7e5c7fea1b39275681f53cf5a3483c4519d9b404
DariaSe <daria.samosledova@icloud.com>
2022-04-10 11:24:51 +0300

    Minor refactoring

--- CoffeeMachine/main.swift
+++ CoffeeMachine/main.swift
@@ -14,10 +14,10 @@ struct Ingredients {
 }
 
 protocol CoffeeDrink {
-    var name: String { get set }
-    var water: Int { get set }
-    var coffee: Int { get set }
-    var milk: Int { get set }
+    var name: String { get }
+    var water: Int { get }
+    var coffee: Int { get }
+    var milk: Int { get }
 }
 
 struct Espresso: CoffeeDrink {
@@ -77,36 +77,46 @@ enum Argument: String {
 
 class CoffeeMachine {
     
-    var isOn: Bool = false
-    var cupsBeforeClean: Int = 5
-    var cupsMade: Int = 0
+    // MARK: - Stored properties
     
-    var waterCapacity: Int = 2000
-    var coffeeCapacity: Int = 500
-    var milkCapacity: Int = 1000
+    private var isOn: Bool = false
     
-    var ingredients: Ingredients = Ingredients(water: 1000, coffee: 300, milk: 1000)
+    private let cupsBeforeClean: Int = 5
+    private var cupsMade: Int = 0
     
-    func turnOn() {
+    private let waterCapacity: Int = 2000
+    private let coffeeCapacity: Int = 500
+    private let milkCapacity: Int = 1000
+    
+    private var ingredients: Ingredients = Ingredients(water: 1000, coffee: 300, milk: 1000)
+    
+    
+    // MARK: - Computed properties
+    
+    private var needsCleaning: Bool { cupsMade == cupsBeforeClean }
+    
+    private var waterStatus: String { "Water: \(ingredients.water)/\(waterCapacity) ml" }
+    private var coffeeStatus: String { "Coffee: \(ingredients.coffee)/\(coffeeCapacity) g" }
+    private var milkStatus: String { "Milk: \(ingredients.milk)/\(milkCapacity) ml"}
+    private var cleaningStatus: String { cupsMade < cupsBeforeClean ? "Needs cleaning after \(cupsBeforeClean - cupsMade) cups" : "Needs cleaning" }
+    
+    private var ingredientsStatus: String { [waterStatus, coffeeStatus, milkStatus].joined(separator: ".") }
+    
+    // MARK: - Methods
+    
+    private func turnOn() {
         isOn = true
         print("Coffeemachine is ready to use.")
         print(ingredientsStatus)
         print(cleaningStatus)
     }
     
-    func turnOff() {
+    private func turnOff() {
         isOn = false
         print("Bye!")
     }
     
-    var waterStatus: String { "Water: \(ingredients.water)/\(waterCapacity) ml" }
-    var coffeeStatus: String { "Coffee: \(ingredients.coffee)/\(coffeeCapacity) g" }
-    var milkStatus: String { "Milk: \(ingredients.milk)/\(milkCapacity) ml"}
-    var cleaningStatus: String { cupsMade < cupsBeforeClean ? "Needs cleaning after \(cupsBeforeClean - cupsMade) cups" : "Needs cleaning" }
-    
-    var ingredientsStatus: String { [waterStatus, coffeeStatus, milkStatus].joined(separator: ".") }
-    
-    func add(_ ingredient: inout Int, amount: Int, maxCapacity: Int, name: String, measurementUnit: String) {
+    private func add(_ ingredient: inout Int, amount: Int, maxCapacity: Int, name: String, measurementUnit: String) {
         let freeSpace = maxCapacity - ingredient
         let amountToAdd = amount <= freeSpace ? amount : freeSpace
         let warningString = amount <= freeSpace ? "" : "Can only add \(amountToAdd) \(measurementUnit) \(name)\n"
@@ -116,18 +126,16 @@ class CoffeeMachine {
         print(warningString, (name.capitalized), ": ", oldStatus, " -> ", newStatus, separator: "", terminator: "\n")
     }
     
-    var needsCleaning: Bool { cupsMade == cupsBeforeClean }
-    
-    func clean() {
+    private func clean() {
         cupsMade = 0
         print("Cleaning completed! Now you can make up to \(cupsBeforeClean) cups.")
     }
     
-    func hasEnoughIngredients(for drink: CoffeeDrink, quantity: Int) -> Bool {
+    private func hasEnoughIngredients(for drink: CoffeeDrink, quantity: Int) -> Bool {
         return ingredients.water >= drink.water * quantity && ingredients.coffee >= drink.coffee * quantity && ingredients.milk >= drink.milk * quantity
     }
     
-    func make(drink: CoffeeDrink, quantity: Int) {
+    private func make(drink: CoffeeDrink, quantity: Int) {
         guard !needsCleaning else {
             print("--Please clean the coffemachine first.")
             return
@@ -139,29 +147,28 @@ class CoffeeMachine {
             }
             return
         }
-        if hasEnoughIngredients(for: drink, quantity: quantity) {
-            ingredients.water -= drink.water * quantity
-            ingredients.coffee -= drink.coffee * quantity
-            ingredients.milk -= drink.milk * quantity
-            cupsMade += quantity
-            let be = quantity == 1 ? "is" : "are"
-            let quantityString = quantity == 1 ? "" : String(quantity)
-            print("Your", quantityString, drink.name, be, "ready!")
-            print(ingredientsStatus)
-            print(cleaningStatus)
-        }
-        else {
+        guard hasEnoughIngredients(for: drink, quantity: quantity) else {
             print("--Not enough ingredients to make \(quantity) \(drink.name).")
             print(ingredientsStatus)
+            return
         }
+        ingredients.water -= drink.water * quantity
+        ingredients.coffee -= drink.coffee * quantity
+        ingredients.milk -= drink.milk * quantity
+        cupsMade += quantity
+        let be = quantity == 1 ? "is" : "are"
+        let quantityString = quantity == 1 ? "" : String(quantity)
+        print("Your", quantityString, drink.name, be, "ready!")
+        print(ingredientsStatus)
+        print(cleaningStatus)
     }
     
-    func printRecipe(of drink: CoffeeDrink) {
+    private func printRecipe(of drink: CoffeeDrink) {
         print("~\(drink.name.capitalized)~")
         print("Water: \(drink.water) ml, coffee: \(drink.coffee) g, milk: \(drink.milk) ml")
     }
     
-    func printUsage() {
+    private func printUsage() {
         let title = "==Coffeemachine usage=="
         let args = "Use full or short arguments names: w == water, c == coffee, m == milk; es == espresso, ca == cappucchino, la == latte"
         let on = "on - To turn on"
@@ -176,6 +183,8 @@ class CoffeeMachine {
         let all = [title, args, on, off, add, check, clean, make, makeCustom, printRecipe, help].joined(separator: "\n")
         print(all)
     }
+    
+    // MARK: - Public API
     
     func handleInput() {
         var shouldQuit = false
@@ -201,19 +210,16 @@ class CoffeeMachine {
                 let prefix = String(ingredientString.prefix(1))
                 guard let ingredient = Argument(rawValue: prefix) else { print("--Incorrect ingredient"); continue input }
                 var amount: Int
-                if args.count == 2 {
-                    amount = Int.max
-                }
-                else if args.count == 3 {
+                switch args.count {
+                case 2: amount = Int.max
+                case 3:
                     let amountString = args[2]
-                    guard let amountInt = Int(amountString) else { print("--Incorrect amount"); continue input }
+                    guard let amountInt = Int(amountString), amountInt > 0 else { print("--Incorrect amount"); continue input }
                     amount = amountInt
-                }
-                else {
+                default:
                     print("--Incorrect arguments count")
                     continue input
                 }
-                guard amount > 0 else { print("--Incorrect amount"); continue input }
                 switch ingredient {
                 case .water:
                     add(&ingredients.water, amount: amount, maxCapacity: waterCapacity, name: "Water", measurementUnit: "ml")
@@ -223,15 +229,14 @@ class CoffeeMachine {
                     add(&ingredients.coffee, amount: amount, maxCapacity: coffeeCapacity, name: "Coffee", measurementUnit: "g")
                 default:
                     print("--Incorrect ingredient")
-                    continue input
                 }
             case .check:
                 guard isOn else { print("--Coffeemechine is off. Run 'on' to turn on"); continue input }
-                if args.count == 1 {
+                switch args.count {
+                case 1:
                     print(ingredientsStatus)
                     print(cleaningStatus)
-                }
-                else if args.count == 2 {
+                case 2:
                     let argumentString = args[1]
                     let prefix = String(argumentString.prefix(1))
                     guard let argument = Argument(rawValue: prefix) else { print("--Incorrect argument"); continue input }
@@ -245,8 +250,7 @@ class CoffeeMachine {
                     default:
                         print("--Incorrect argument")
                     }
-                }
-                else {
+                default:
                     print("--Incorrect arguments count")
                 }
             case .clean:
@@ -254,22 +258,16 @@ class CoffeeMachine {
                 clean()
             case .make:
                 guard isOn else { print("--Coffeemechine is off. Run 'on' to turn on"); continue input }
-                if args.count == 2 || args.count == 3 {
+                switch args.count {
+                case 2, 3:
                     let drinkString = args[1]
                     let prefix = String(drinkString.prefix(2))
                     guard let drink = Argument(rawValue: prefix) else { print("--Incorrect drink name"); continue input }
-                    var quantity: Int
-                    if args.count == 2 {
-                        quantity = 1
-                    }
-                    else if args.count == 3 {
+                    var quantity = 1
+                    if args.count == 3 {
                         let cupsNumberString = args[2]
                         guard let cupsNumber = Int(cupsNumberString), cupsNumber > 0 else { print("--Incorrect cups number"); continue input }
                         quantity = cupsNumber
-                    }
-                    else {
-                        print("--Incorrect arguments count")
-                        continue input
                     }
                     switch drink {
                     case .espresso:
@@ -281,8 +279,7 @@ class CoffeeMachine {
                     default:
                         print("--Incorrect drink name")
                     }
-                }
-                else if args.count == 4 {
+                case 4:
                     let amounts = args.suffix(3).compactMap({Int($0)}).filter({$0 > 0})
                     guard amounts.count == 3 else { print("--Incorrect input"); continue input }
                     let coffeeAmount = amounts[0]
@@ -290,10 +287,7 @@ class CoffeeMachine {
                     let milkAmount = amounts[2]
                     let drink = CustomDrink(name: "drink", water: waterAmount, coffee: coffeeAmount, milk: milkAmount)
                     make(drink: drink, quantity: 1)
-                }
-                else {
-                    print("--Incorrect input")
-                    continue input
+                default: print("--Incorrect input")
                 }
             case .recipe:
                 guard isOn else { print("--Coffeemechine is off. Run 'on' to turn on"); continue input }
